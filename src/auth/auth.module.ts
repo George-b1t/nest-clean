39a965a8bf1e6f6d0adfee6d2e7fdea294bcb7e4
George <george.soares509@gmail.com>
2023-12-26 21:21:13 -0300

    jwt auth

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { Env } from 'src/env'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory: (configService: ConfigService<Env>) => {
+        const privateKey = configService.get('JWT_PRIVATE_KEY')
+        const publicKey = configService.get('JWT_PUBLIC_KEY')
+
+        return {
+          signOptions: {
+            algorithm: 'RS256',
+          },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
+        }
+      },
+    }),
+  ],
+})
+export class AuthModule {}
