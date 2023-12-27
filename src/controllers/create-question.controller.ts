21301f37b1e248990f13da4d44d7651ef4678ae8
George <george.soares509@gmail.com>
2023-12-27 19:59:30 -0300

    auth and create question

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,53 @@
+import { Controller, HttpCode, Post, UseGuards, Body } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+
+    const slug = this.titleToSlug(title)
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug,
+      },
+    })
+  }
+
+  private titleToSlug(title: string): string {
+    return title
+      .toLowerCase() // Convert to lowercase
+      .replace(/[\s_]+/g, '-') // Replace spaces and underscores with dashes
+      .replace(/[^\w-]+/g, '') // Remove all non-word chars
+      .replace(/--+/g, '-') // Replace multiple dashes with single dash
+      .replace(/^-+/, '') // Trim dashes from the start
+      .replace(/-+$/, '') // Trim dashes from the end
+  }
+}
