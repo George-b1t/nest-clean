3426013a66392c945d500d3b1371dc892410206d
George <george.soares509@gmail.com>
2023-12-29 17:07:13 -0300

    iniciando testes e2e

--- src/controllers/fetch-recent-questions.controller.ts
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, HttpCode, UseGuards, Get, Query } from '@nestjs/common'
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { PrismaService } from 'src/prisma/prisma.service'
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { PrismaService } from '@/prisma/prisma.service'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
 import { z } from 'zod'
 
 const pageQueryParamSchema = z
